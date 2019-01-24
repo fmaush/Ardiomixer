90ce298de87d17ad1d2b5eed738302cfc1365ede
fmaush <46869662+fmaush@users.noreply.github.com>
2019-01-24 12:19:14 +0100

    Update serial_encoder_1.0.0.ino

--- Arduino/serial_encoder_1.0.0/serial_encoder_1.0.0.ino
+++ Arduino/serial_encoder_1.0.0/serial_encoder_1.0.0.ino
@@ -1,53 +1,54 @@
-#define encoderOutA 2 // CLK pin of Rotary Enocoder
-#define encoderOutB 3 // DT pin of Rotary Enocoder
-#define encoderOutC 4 // Button Press
+#define Rotary_Encoder_Out_A 1  // CLK pin of Rotary Enocoder
+#define Rotary_Encoder_Out_B 2  // DT pin of Rotary Enocoder
+#define Rotary_Encoder_Button 3 // Button Press
 boolean gerade = true;
-int presentState;
-int previousState;
-long penis2 = 500;
-long langerpenis = 20000;
+int Rotary_Encoder_Present_State;
+int Rotary_Encoder_Previous_State;
+int RotaryEncoder_Button_Previous
 
+    void
+    setup()
+{
+  pinMode(Rotary_Encoder_Out_A, INPUT);
+  pinMode(Rotary_Encoder_Out_B, INPUT);
+  pinMode(Rotary_Encoder_Button, INPUT_PULLUP);
 
-void setup() {
-  pinMode (encoderOutA, INPUT);
-  pinMode (encoderOutB, INPUT);
-  pinMode (encoderOutC, INPUT_PULLUP);
-
-
-  Serial.begin (9600);
-  previousState = digitalRead(encoderOutA);   // Get current state of the encoderOutA
+  Serial.begin(9600);
+  Rotary_Encoder_Previous_State = digitalRead(Rotary_Encoder_Out_A); // Get current state of the Rotary_Encoder_Out_A
 }
-void loop() {
-  int penis = digitalRead(encoderOutC);
-  presentState = digitalRead(encoderOutA);
-  if (presentState != previousState)
+void loop()
+{
+  int button = digitalRead(Rotary_Encoder_Button); //auslesen von Button press
+  Rotary_Encoder_Present_State = digitalRead(Rotary_Encoder_Out_A);
+  if (Rotary_Encoder_Present_State != Rotary_Encoder_Previous_State)
   {
-    if (gerade) {
-      if (digitalRead(encoderOutB) != presentState)
+    if (gerade)
+    {
+      if (digitalRead(Rotary_Encoder_Out_B) != Rotary_Encoder_Present_State)
       {
         Serial.println("+1");
-        gerade = false ;
+        gerade = false;
       }
-      else {
+      else
+      {
         Serial.println("-1");
         gerade = false;
-
       }
-    } else {
+    }
+    else
+    {
       gerade = true;
     }
   }
-  previousState = presentState; // Replace previous state of the encoderOutA with the current state
+  Rotary_Encoder_Previous_State = Rotary_Encoder_Present_State; // Replace previous state of the Rotary_Encoder_Out_A with the current state
 
-
-  if (penis == LOW && penis2 >= langerpenis)  {
+  if (button == LOW && Rotary_Encoder_ButtonTimingCheck >= Rotary_Encoder_ButtonDelay)
+  {
     Serial.println("Button");
-    penis2 = 0;
-
-
+    Rotary_Encoder_ButtonTimingCheck = 0;
   }
-  else if (penis2 < langerpenis) {
-    penis2 ++;
+  else if (Rotary_Encoder_ButtonTimingCheck < Rotary_Encoder_ButtonDelay)
+  {
+    Rotary_Encoder_ButtonTimingCheck++;
   }
-
 }
